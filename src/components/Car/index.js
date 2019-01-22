00face8eac1f0125e138c6ceaa784f516c23c43f
jason-json <jasonfi28@gmail.com>
2019-01-22 14:20:04 -0600

    Header Section
    Title update by props and route

--- src/components/Car/index.js
+++ src/components/Car/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import Carousel from './Carousel';
-import Versions from './Versions';
-import Form from './Form';
-
+import React from "react";
+import Carousel from "./Carousel";
+import Versions from "./Versions";
+import Form from "./Form";
+import logo from "../../lib/img/logo.jpg";
 const Car = props => {
   return (
     <main>
-      
+      <section className="header_section">
+        <div className="header_section_logo">
+          <img src={logo} alt="Nissan" />
+        </div>
+        <div className="header_section_item">
+          <h1>{props.title}</h1>
+        </div>
+        <div className="header_section_item"/>
+      </section>
     </main>
   );
 };
